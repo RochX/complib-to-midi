8217c322d8747335d0bbfbf8bff59391d06fb43a
Xavier Silva <xavierwsilva+git@gmail.com>
2026-08-01 18:39:45 -0400

    Add get_rows_short and function docs

--- complib_api.py
+++ complib_api.py
@@ -3,8 +3,32 @@ import requests
 # parameter type is either "method" or "composition"
 
 def api_request(complib_id, type):
+  """
+  Gets the reponse from the complib API.
+
+  Parameters:
+    complib_id: ID to fetch on complib.
+    type: either "method" or "composition"
+  """
   return requests.get(f"https://api.complib.org/{type}/{complib_id}/rows")
 
 def get_rows(complib_id, type):
+  """
+  Gets the full API JSON response.
+
+  Includes rows, the spoken calls, and row analysis.
+  The result returns 2 rows of rounds before the start of the method/composition.
+
+  See complib.org/api for more details on row analysis.
+  """
   r = api_request(complib_id, type)
   return r.json()['rows']
+
+def get_rows_short(complib_id, type):
+  """
+  Gets the rows from the API JSON response.
+
+  The result returns 2 rows of rounds before the start of the method/composition.
+  """
+  r = api_request(complib_id, type)
+  return [x[0] for x in r.json()['rows']]
--- tests.py
+++ tests.py
@@ -2,14 +2,17 @@ import unittest
 
 import complib_api
 
-class TestRowsFromComplib(unittest.TestCase):
+class TestComplibAPI(unittest.TestCase):
   def setUp(self):
+    self.hunt_minimus = ['1234','1234','2143','2413','4231','4321','3412','3142','1324','1234']
     self.hunt_minimus_json = [["1234","Go Original Minimus","8"],["1234","","16"],["2143","","264"],["2413","","20"],["4231","","268"],["4321","","16"],["3412","","264"],["3142","","20"],["1324","That's all; Stand","268"],["1234","","0"]]
     return super().setUp()
 
-  def test_get_method(self):
+  def test_get_rows(self):
     self.assertEqual(complib_api.get_rows(27808, "method"),self.hunt_minimus_json)
 
+  def test_get_rows_short(self):
+    self.assertEqual(complib_api.get_rows_short(27808, "method"),self.hunt_minimus)
 
 if __name__ == "__main__":
   unittest.main()
